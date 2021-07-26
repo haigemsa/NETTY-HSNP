fe95224b2024b6515988ca81c3994875b9c2c64f
cw <haigemsa@qq.com>
2021-07-27 00:04:01 +0800

    update

--- src/main/java/com/grich/hsnp/entity/AdviceStatusEntity.java
+++ src/main/java/com/grich/hsnp/entity/AdviceStatusEntity.java
@@ -27,27 +27,8 @@ public class AdviceStatusEntity {
      */
     private volatile String channelId;
 
-    /**
-     * 目前使用用户ID
-     */
-    private volatile String userId;
-    /**
-     * 设备使用时间
-     */
-    private volatile Integer useTime;
-    /**
-     * 设备使用开始时间
-     */
-    private volatile Date startTime;
-    /**
-     * 设备使用金额
-     */
-    private volatile BigDecimal money;
 
-    /**
-     * 设备类型：DS：洗浴区   DM：烘干区(小中)  DD：烘干区(大)  DA：造型区
-     */
-    private volatile String type;
+
 
     /**
      * 目前在店人数
@@ -64,24 +45,5 @@ public class AdviceStatusEntity {
      * 是否在运行
      */
     private volatile boolean isRunning;
-
-
-    public void clear(){
-        this.userId = null;
-        this.useTime = null;
-        this.code = null;
-        this.money = null;
-        this.startTime = null;
-        this.isRunning = false;
-    }
-
-    public void init(){
-        this.userId = null;
-        this.useTime = 0;
-        this.code = null;
-        this.money = BigDecimal.ZERO;
-        this.startTime = null;
-        this.isRunning = false;
-    }
 
 }
